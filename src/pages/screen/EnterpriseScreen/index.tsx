0634ca906736c9441c6019a25bc7df050f9f9c06
uuz <ncysatnaf@gmail.com>
2020-02-28 16:55:33 +0800

    add fullscreen

--- src/pages/screen/EnterpriseScreen/index.tsx
+++ src/pages/screen/EnterpriseScreen/index.tsx
@@ -1,19 +1,23 @@
-import React from "react";
-import { useObserver } from "mobx-react-lite";
+import React, { useRef } from "react";
+import { useObserver, useLocalStore } from "mobx-react-lite";
 import { EnterpriseMap } from "./Map";
 import { EnterpriseScreenGasChart } from "./GasChart";
-import {Header} from "./header";
+import { Header } from "./header";
+import { Button } from "antd";
 
 export const EnterpriseScreenPage = () => {
+  const fullScreenRef = useRef<HTMLDivElement>(null);
+
   return useObserver(() => (
-    <div className="w-full h-full" style={{ backgroundColor: "#0d1b28"}}>
-      <Header/>
+    <div ref={fullScreenRef} className="w-full h-full" style={{ backgroundColor: "#0d1b28" }}>
+      <Header />
       <div style={{ width: "46vw", height: "46vh", margin: "0 auto" }}>
         <EnterpriseMap />
       </div>
       <div style={{ width: "23vw", height: "34vh" }}>
         <EnterpriseScreenGasChart />
       </div>
+      <Button onClick={() => fullScreenRef.current?.requestFullscreen()}>全屏</Button>
     </div>
   ));
 };
