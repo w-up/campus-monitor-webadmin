690ae0293a1c5555541b6f06e48c4b3449268bf5
Shuo <shuo@iotex.io>
2020-02-28 23:17:59 +0800

    gas table right playing

--- src/pages/screen/EnterpriseScreen/right/GasTableDynamic.tsx
+++ src/pages/screen/EnterpriseScreen/right/GasTableDynamic.tsx
@@ -1,7 +1,13 @@
 import React from 'react';
 import {useObserver} from "mobx-react-lite";
+import {CarouselProvider, Dot, DotGroup, Slide, Slider} from "pure-react-carousel";
+import {GasTableStore} from "../../../../stores/screen/GasTableStore";
+import {useStore} from "../../../../stores";
 
 export const GasTableDynamic = () => {
+  const {
+    screen: {gasTable: GasTableStore}
+  } = useStore();
 
   return useObserver(() => (
       <div className="topright">
@@ -14,6 +20,33 @@ export const GasTableDynamic = () => {
             <div>限值</div>
             <div>超标率(%)</div>
           </div>
+          <CarouselProvider
+            naturalSlideWidth={100}
+            naturalSlideHeight={125}
+            totalSlides={3}
+            isPlaying={true}
+          >
+            <Slider>
+              <Slide index={0}>{GasTableStore.list.map((item) => {
+                return (
+                  <div className="listitem tabtitle">
+                    <div>{item.name}</div>
+                    <div>{item.dataName}</div>
+                    <div>{item.num + 'ppm'}</div>
+                    <div>{item.maxNum}</div>
+                    <div>{item.point}</div>
+                  </div>
+                )
+              })}</Slide>
+              <Slide index={1} className="text-white">I am the second Slide.</Slide>
+              <Slide index={2} className="text-white">I am the third Slide.</Slide>
+            </Slider>
+            <DotGroup className="text-center">
+              <Dot slide={0} className="text-white sliderDotButton"> </Dot>
+              <Dot slide={1} className="text-white sliderDotButton"> </Dot>
+              <Dot slide={2} className="text-white sliderDotButton"> </Dot>
+            </DotGroup>
+          </CarouselProvider>
         </div>
       </div>
     )
