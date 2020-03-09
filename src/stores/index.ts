482c9c3a33de41149c1615ec8de10a86f139ec62
uuz <ncysatnaf@gmail.com>
2020-03-09 19:23:30 +0800

    draw arrow

--- src/stores/index.ts
+++ src/stores/index.ts
@@ -8,12 +8,14 @@ import { GasTableStore } from "./screen/GasTableStore";
 import { SewageTableStore } from "./screen/SewageTableStore";
 import { DrawMapStore } from "./map/DrawMapStore";
 import { MapMonitorStore } from "./mapMonitor/index";
+import { DynamicSourceStore } from "./dynamicSource/index";
 
 export const store = {
   menu: new MenuStore(),
   auth: new AuthStore(),
   config: new ConfigStore(),
   mapMonitor: new MapMonitorStore(),
+  dynamicSource: new DynamicSourceStore(),
   map: {
     drawMap: new DrawMapStore()
   },
