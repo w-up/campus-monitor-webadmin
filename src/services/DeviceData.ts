de54690fafbaf6683ddbbd64828e2196e93e2c98
uuz <ncysatnaf@gmail.com>
2020-03-11 18:07:21 +0800

    clean code

--- src/services/DeviceData.ts
+++
@@ -1,7 +0,0 @@
-import { GET, POST } from "../utils/request";
-
-export default {
-  getAllPMDataBySitesAndPMs(data: { current: number; pmCodeList: string[]; siteIdList: []; size: number }) {
-    return GET("/device-data/getAllPMDataBySitesAndPMs", data);
-  }
-};
--- src/services/MapConfig.ts
+++
@@ -1,7 +0,0 @@
-import { GET, POST } from "../utils/request";
-
-export default {
-  getMapConfigLogin() {
-    return GET("/map-config/getMapConfigLogin");
-  }
-};
