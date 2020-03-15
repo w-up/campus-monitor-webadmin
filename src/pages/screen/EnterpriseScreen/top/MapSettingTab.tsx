cca7e6ffbe46247388d14a8bb06684afab2b6d8d
Shuo <shuo@iotex.io>
2020-03-15 12:41:44 +0800

    setteng box

--- src/pages/screen/EnterpriseScreen/top/MapSettingTab.tsx
+++ src/pages/screen/EnterpriseScreen/top/MapSettingTab.tsx
@@ -1,16 +1,17 @@
 import React from "react";
-import { useLocalStore, useObserver } from "mobx-react-lite";
-import { Form, Icon, Button } from "antd";
-import { WrappedFormUtils } from "antd/lib/form/Form";
+import {useLocalStore, useObserver} from "mobx-react-lite";
+import {Form, Icon, Button} from "antd";
+import {WrappedFormUtils} from "antd/lib/form/Form";
 import Input from "antd/lib/input";
 import Upload from "antd/lib/upload";
 import Modal from "antd/lib/modal";
-import { useStore } from "stores";
+import {useStore} from "stores";
+import {Scrollbars} from "react-custom-scrollbars";
 
-export const MapSettingTab = Form.create()(({ form }: { form: WrappedFormUtils }) => {
-  const { getFieldDecorator } = form;
+export const MapSettingTab = Form.create()(({form}: { form: WrappedFormUtils }) => {
+  const {getFieldDecorator} = form;
   const {
-    screen: { enterpriseScreenMap }
+    screen: {enterpriseScreenMap}
   } = useStore();
 
   const store = useLocalStore(() => ({
@@ -40,7 +41,7 @@ export const MapSettingTab = Form.create()(({ form }: { form: WrappedFormUtils }
       store.previewVisible = true;
       store.previewImage = file.url || file.preview;
     },
-    handleChange: async ({ file }) => {
+    handleChange: async ({file}) => {
       const formData = new FormData();
       formData.append("files[]", file);
       console.log(file);
@@ -63,7 +64,7 @@ export const MapSettingTab = Form.create()(({ form }: { form: WrappedFormUtils }
   }));
   const uploadButton = (
     <div>
-      <Icon type="plus" />
+      <Icon type="plus"/>
       <div className="ant-upload-text">Upload</div>
     </div>
   );
@@ -71,46 +72,58 @@ export const MapSettingTab = Form.create()(({ form }: { form: WrappedFormUtils }
   return useObserver(() => (
     <div className="">
       <Form {...store.formItemLayout} onSubmit={store.handleSubmit}>
-        <Form.Item label="俯视角度">
-          <Input type="number" value={enterpriseScreenMap.curMapConfig.highAngle} onChange={e => (enterpriseScreenMap.curMapConfig.highAngle = Number(e.target.value))} style={{ width: "80%" }} />
-        </Form.Item>
-        <div>
-          <Form.Item label="中心坐标">
+        <Scrollbars style={{height: 320}}>
+          <Form.Item label="俯视角度">
+            <Input type="number" value={enterpriseScreenMap.curMapConfig.highAngle}
+                   onChange={e => (enterpriseScreenMap.curMapConfig.highAngle = Number(e.target.value))}
+                   style={{width: "80%"}}/>
+          </Form.Item>
+          <div>
+            <Form.Item label="中心坐标">
             <span>
               <Input
                 type="number"
                 value={enterpriseScreenMap.curMapConfig.longitude}
                 onChange={e => (enterpriseScreenMap.curMapConfig.longitude = Number(e.target.value))}
-                style={{ width: "45%", marginRight: "5%" }}
+                style={{width: "45%", marginRight: "5%"}}
               />
-              <Input type="number" value={enterpriseScreenMap.curMapConfig.latitude} onChange={e => (enterpriseScreenMap.curMapConfig.latitude = Number(e.target.value))} style={{ width: "45%" }} />
+              <Input type="number" value={enterpriseScreenMap.curMapConfig.latitude}
+                     onChange={e => (enterpriseScreenMap.curMapConfig.latitude = Number(e.target.value))}
+                     style={{width: "45%"}}/>
             </span>
+            </Form.Item>
+          </div>
+          <Form.Item label="缩放比例">
+            <Input type="number" value={enterpriseScreenMap.curMapConfig.zoom}
+                   onChange={e => (enterpriseScreenMap.curMapConfig.zoom = Number(e.target.value))}
+                   style={{width: "80%"}}/>
           </Form.Item>
-        </div>
-        <Form.Item label="缩放比例">
-          <Input type="number" value={enterpriseScreenMap.curMapConfig.zoom} onChange={e => (enterpriseScreenMap.curMapConfig.zoom = Number(e.target.value))} style={{ width: "80%" }} />
-        </Form.Item>
-        <Form.Item label="旋转角度">
-          <Input
-            type="number"
-            value={enterpriseScreenMap.curMapConfig.rotationAngle}
-            onChange={e => (enterpriseScreenMap.curMapConfig.rotationAngle = Number(e.target.value))}
-            style={{ width: "80%" }}
-          />
-        </Form.Item>
-        <Form.Item label="3D仿真地图">
-          {getFieldDecorator("type", { initialValue: 1 })(
-            <div className="clearfix">
-              <Upload listType="picture-card" showUploadList={false} beforeUpload={store.beforeUpload} onPreview={store.handlePreview} onChange={store.handleChange}>
-                {enterpriseScreenMap.curMapConfig.picUrl ? <img src={enterpriseScreenMap.curMapConfig.picUrl} alt="avatar" style={{ width: "100%" }} /> : uploadButton}
-              </Upload>
-              <Modal visible={store.previewVisible} footer={null} onCancel={store.handleCancel}>
-                <img alt="example" style={{ width: "100%" }} src={store.previewImage} />
-              </Modal>
-            </div>
-          )}
-        </Form.Item>
-        <div className="setting-box-footer">
+          <Form.Item label="旋转角度">
+            <Input
+              type="number"
+              value={enterpriseScreenMap.curMapConfig.rotationAngle}
+              onChange={e => (enterpriseScreenMap.curMapConfig.rotationAngle = Number(e.target.value))}
+              style={{width: "80%"}}
+            />
+          </Form.Item>
+          <Form.Item label="3D仿真地图">
+            {getFieldDecorator("type", {initialValue: 1})(
+              <div className="clearfix">
+                <Upload listType="picture-card" showUploadList={false} beforeUpload={store.beforeUpload}
+                        onPreview={store.handlePreview} onChange={store.handleChange}>
+                  {enterpriseScreenMap.curMapConfig.picUrl ?
+                    <img src={enterpriseScreenMap.curMapConfig.picUrl} alt="avatar"
+                         style={{width: "100%"}}/> : uploadButton}
+                </Upload>
+                <Modal visible={store.previewVisible} footer={null} onCancel={store.handleCancel}>
+                  <img alt="example" style={{width: "100%"}} src={store.previewImage}/>
+                </Modal>
+              </div>
+            )}
+          </Form.Item>
+
+        </Scrollbars>
+        <div className="setting-box-footer text-center mt-4">
           <Button type="primary" size="default" onClick={enterpriseScreenMap.saveMapConfig}>
             确定
           </Button>
@@ -120,5 +133,6 @@ export const MapSettingTab = Form.create()(({ form }: { form: WrappedFormUtils }
         </div>
       </Form>
     </div>
-  ));
+  ))
+    ;
 });
