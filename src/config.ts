ca4f2f6a89d486d58494896a51ed087bd3901f0b
uuz <ncysatnaf@gmail.com>
2020-03-23 16:20:42 +0800

    load api url from query

--- src/config.ts
+++ src/config.ts
@@ -1,3 +1,6 @@
+const query = new URLSearchParams(window.location.search);
+const api = query.get("api");
+
 export const globalConfig = {
-  apiEndpoint: process.env.NODE_ENV == "development" ? "http://69uu2r.natappfree.cc/" : "http://69uu2r.natappfree.cc/"
+  apiEndpoint: api ? api : process.env.NODE_ENV == "development" ? "?api=http://69uu2r.natappfree.cc/" : "http://gypark.admin.iisu.cn/api/"
 };
