ca4c2649bdbdb06d2f01b28820b3621e28d69932
uuz <ncysatnaf@gmail.com>
2020-03-23 16:22:01 +0800

    fix url

--- src/config.ts
+++ src/config.ts
@@ -2,5 +2,5 @@ const query = new URLSearchParams(window.location.search);
 const api = query.get("api");
 
 export const globalConfig = {
-  apiEndpoint: api ? api : process.env.NODE_ENV == "development" ? "?api=http://69uu2r.natappfree.cc/" : "http://gypark.admin.iisu.cn/api/"
+  apiEndpoint: api ? api : process.env.NODE_ENV == "development" ? "http://69uu2r.natappfree.cc/" : "http://69uu2r.natappfree.cc/"
 };
