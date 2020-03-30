8bdd893eef51ff5e325f4e9a880e3c80c2c009b9
petegao <petegao@tencent.com>
2020-03-30 09:29:01 +0800

    change api

--- src/config.ts
+++ src/config.ts
@@ -2,5 +2,5 @@ const query = new URLSearchParams(window.location.search);
 const api = query.get("api");
 
 export const globalConfig = {
-  apiEndpoint: api ? api : process.env.NODE_ENV == "development" ? "http://69uu2r.natappfree.cc/" : "http://69uu2r.natappfree.cc/"
+  apiEndpoint: api ? api : process.env.NODE_ENV == "development" ? "http://2ejnkz.natappfree.cc/" : "http://2ejnkz.natappfree.cc/"
 };
