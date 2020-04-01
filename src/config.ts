6fc8e926dd42e317702509189c16cbd6968b8469
uuz <ncysatnaf@gmail.com>
2020-04-01 09:29:56 +0800

    update api config

--- src/config.ts
+++ src/config.ts
@@ -2,6 +2,6 @@ const query = new URLSearchParams(window.location.search);
 const api = query.get("api");
 
 export const globalConfig = {
-  apiEndpoint: api ? api : process.env.NODE_ENV == "development" ? "http://2ejnkz.natappfree.cc/" : "http://2ejnkz.natappfree.cc/"
+  apiEndpoint: api ? api : process.env.NODE_ENV == "development" ? "http://z69yih.natappfree.cc" : "http://z69yih.natappfree.cc"
   // wsEndpoint: process.env.NODE_ENV == "development": ""
 };
