cb23cf4866a19ad8dc48b56c5c8a03e9f03d7927
uuz <ncysatnaf@gmail.com>
2020-04-13 15:32:46 +0800

    fix date picker

--- src/pages/MapMonitor/RuntimeMonitor.tsx
+++ src/pages/MapMonitor/RuntimeMonitor.tsx
@@ -46,137 +46,6 @@ export const RuntimeMonitor = Form.create()(({ form }: { form: WrappedFormUtils 
         }
       });
     },
-    get options() {
-      return {
-        //   标题配置
-        title: {
-          text: "24小时排放趋势图",
-          textStyle: {
-            color: "#88A8C5FF",
-            fontSize: "14",
-            fontWeight: "normal",
-          },
-          x: "center",
-          y: "20px",
-          padding: [5, 20],
-        },
-        // 提示配置
-        tooltip: {
-          trigger: "axis",
-          backgroundColor: "rgba(38,95,163,0.6)",
-          padding: 10,
-          textStyle: {
-            color: "#88A8C5",
-            fontSize: 10,
-          },
-          alwaysShowContent: {
-            show: true,
-          },
-          formatter: (params: any, ticket: any, callback: any) => {
-            let showHtml = "";
-            for (var i = 0; i < params.length; i++) {
-              var list = {} as any;
-              //x轴名称
-              var name = params[i].seriesName;
-              //名称
-              var text = params[i].axisValue;
-              //值
-              var value = params[i].data.value;
-              var unit = params[i].data.unit;
-
-              if (Number(value) > 0) {
-                showHtml += `
-            <div style="display:flex;align-items: center;">
-            <div style="margin-right:10px;width:10px;height:1px;border:1px solid ${constant.seriesColors[i]};background:${constant.seriesColors[i]}"></div>
-            <div>${name}</div>
-            <div style="color:#04F9CC;text-align:right;display:inline-block;margin-left:15px">${value ? utils.number.toPrecision(value) + unit : ""}</div>
-          </div>
-          `;
-              }
-            }
-            return `<div style="color: #04F9CC;text-align:left;line-height:20px">${text} 日均</div>
-            <div style="color:#88A8C5;text-align:left;font-size:10px;background:rgba(11,36,69,0.6);padding:5px;border-radius:5px;margin-top:5px;">
-            ${showHtml}
-            </div>
-          </div>`;
-          },
-        },
-        // 上册图列配置
-        legend: {
-          data: this.siteData?.dataTrend.map((i) => i.pmName),
-          textStyle: {
-            fontSize: 10,
-            color: "#88A8C5", // 图例文字颜色
-          },
-          // y:"-10px",
-        },
-        grid: {
-          top: "25%",
-          left: "4%",
-          right: "2%",
-          bottom: "0%",
-          containLabel: true,
-        },
-        xAxis: {
-          type: "category",
-          axisLabel: {
-            textStyle: {
-              color: "rgba(136,168,197,0.5)",
-              fontSize: "10",
-            },
-          },
-          data: this.siteData?.dataTrend[0].points.map((i) => i.time),
-        },
-        yAxis: {
-          name: "（mg/m³）",
-          nameTextStyle: {
-            color: "rgba(136,168,197,0.5)",
-            align: "center",
-            verticalAlign: "middle",
-            padding: [5, 0, 15, 20],
-          },
-          type: "value",
-          // min: 1,
-          // max: 100,
-          splitNumber: 3,
-          axisLabel: {
-            textStyle: {
-              color: "rgba(136,168,197,0.5)",
-              fontSize: "10",
-            },
-          },
-          //   分割线
-          splitLine: {
-            lineStyle: {
-              color: "rgba(101,198,231,0.2)",
-            },
-          },
-          //   刻度线
-          axisLine: {
-            show: false,
-          },
-        },
-        series: this.siteData?.dataTrend.map((item, index) => ({
-          name: item.pmName,
-          type: "line",
-          data: item.points.map((i) => ({
-            value: Number(i.collectValue),
-            unit: i.unit,
-          })),
-          itemStyle: {
-            normal: {
-              color: constant.seriesColors[index], //改变折线点的颜色
-              lineStyle: {
-                color: constant.seriesColors[index], //改变折线颜色
-              },
-            },
-          },
-          symbol: "circle", //设定为实心点
-          symbolSize: 6, //设定实心点的大小
-        })),
-      };
-    },
-
     get monitorTable() {
       return {
         dataSource: this.siteData?.realTimeData,
@@ -212,6 +81,12 @@ export const RuntimeMonitor = Form.create()(({ form }: { form: WrappedFormUtils 
       {
         title: "排名",
         dataIndex: "ranking",
+      },
+      {
+        title: "监测类型",
+        dataIndex: "monitorType",
+        align: "center",
+        render: (text, record) => <div className={record.siteId == store.curSiteId ? "primary-button-text-dark" : "primary-text-color"}>{text}</div>,
       },
       {
         title: "站点",
