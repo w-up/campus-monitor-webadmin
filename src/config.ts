53cdc50fb9b96cd139720546ca2eb6d9661430d1
uuz <ncysatnaf@gmail.com>
2020-05-25 20:25:25 +0800

    fix bug

--- src/config.ts
+++ src/config.ts
@@ -2,6 +2,6 @@ const query = new URLSearchParams(window.location.search);
 const api = query.get("api");
 
 export const globalConfig = {
-  apiEndpoint: api ? api : process.env.NODE_ENV == "development" ? "http://39.100.203.1:8898" : "http://39.100.203.1:8898",
-  wsEndpoint: process.env.NODE_ENV == "development" ? "ws://39.100.203.1:8898/webSocketServer/" : "ws://39.100.203.1:8898/webSocketServer/",
+  apiEndpoint: api ? api : process.env.NODE_ENV == "development" ? "http://47.101.175.184:8898" : "http://47.101.175.184:8898",
+  wsEndpoint: process.env.NODE_ENV == "development" ? "ws://47.101.175.184:8898/webSocketServer/" : "ws://47.101.175.184:8898/webSocketServer/",
 };
